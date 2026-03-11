e9b56e19df603f0617c82689b9a498fc453e2671
Tharun-Pasumarthi <tharunplatinum06@gmail.com>
2026-03-11 22:33:38 +0530

    Capacitor Android setup: standalone APK with status bar, safe area, camera permissions

---
+++ android/app/src/main/java/com/pawnvault/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pawnvault.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Capacitor } from '@capacitor/core'
+import { StatusBar, Style } from '@capacitor/status-bar'
+import { SplashScreen } from '@capacitor/splash-screen'
 import App from './App'
 import './styles/global.css'
+
+// Native app initialization
+if (Capacitor.isNativePlatform()) {
+  StatusBar.setBackgroundColor({ color: '#6366f1' })
+  StatusBar.setStyle({ style: Style.Light })
+  StatusBar.setOverlaysWebView({ overlay: false })
+  SplashScreen.hide()
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
